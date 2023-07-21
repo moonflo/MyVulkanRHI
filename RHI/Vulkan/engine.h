fe83ac25d5bfa36969de20d8d099a343aab39d0e
Xuepu Zeng <2307665474zxp@gmail.com>
2023-07-21 14:19:32 +0800

    add glm

--- RHI/Vulkan/engine.h
+++ RHI/Vulkan/engine.h
@@ -1,43 +1,36 @@
 #pragma once
+#include "VulkanConfig.h"
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.hpp>
-/*
-* including the prebuilt header from the lunarg sdk will load
-* most functions, but not all.
-* 
-* Functions can also be dynamically loaded, using the call
-* 
-* PFN_vkVoidFunction vkGetInstanceProcAddr(
-    VkInstance                                  instance,
-    const char*                                 pName);
 
- or
-
- PFN_vkVoidFunction vkGetDeviceProcAddr(
-	VkDevice                                    device,
-	const char*                                 pName);
-
-	We will look at this later, once we've created an instance and device.
-*/
-
-class Engine {
+class Engine
+{
 
 public:
-
 	Engine();
 
 	~Engine();
 
 private:
-
-	//whether to print debug messages in functions
+	// whether to print debug messages in functions
 	bool debugMode = true;
 
-	//glfw window parameters
-	int width{ 640 };
-	int height{ 480 };
-	GLFWwindow* window{ nullptr };
+	// glfw window parameters
+	int width{640};
+	int height{480};
+	GLFWwindow *window{nullptr};
 
-	//glfw setup
+	// vulkan instance
+	vk::Instance instance{nullptr};
+
+	// glfw setup
+	/**
+		Build the GLFW window.
+	*/
 	void build_glfw_window();
+
+	// instance setup
+	/**
+		Create the Vulkan instance.
+	*/
+	void make_instance();
 };
